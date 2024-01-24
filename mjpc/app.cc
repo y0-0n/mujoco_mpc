d4610b3e8fe39f697544287261a7d1988059c94f
yoon0-0 <yoonbyung-chai@korea.ac.kr>
2024-01-24 11:22:51 +0900

    Not add noise to root orientation and position

--- mjpc/app.cc
+++ mjpc/app.cc
@@ -165,7 +165,7 @@ void controller(const mjModel* m, mjData* data) {
   // yoon0-0
   if (!sim->agent->allocate_enabled && sim->uiloadrequest.load() == 0 &&
       sim->qpos_noise_std) { 
-    for (int j = 0; j < sim->m->nq; j++) {
+    for (int j = 7; j < sim->m->nq; j++) {
       data->qpos[j] += qposnoise[j];
     }
   }
@@ -525,7 +525,7 @@ void GetMotionJson(std::string motion_path, std::shared_ptr<mjpc::Agent> agent) 
   std::vector<std::vector<double>> motion_vector_qvel(data["length"], std::vector<double> (0, 0));
   
   // 2d array json parsing
-  double height_offset = 0.25;
+  double height_offset = 0.0;
   int n = 0;
   for (auto it=data["qpos"].begin();it!=data["qpos"].end();++it) {
     int m = 0;
@@ -660,7 +660,7 @@ MjpcApp::MjpcApp(std::vector<std::shared_ptr<mjpc::Task>> tasks, int task_id) {
   sim->agent->PlotInitialize();
   // motion
   // yoon0-0
-  GetMotionJson("/home/yoonbyeong/Dev/mujoco_mpc/mjpc/tasks/smpl/smplrig_cmu_walk_16_15.json", sim->agent);
+  GetMotionJson("/home/yoonbyeong/Dev/mujoco_mpc/mjpc/tasks/smpl/smplrig_cmu_walk_16_15_zpos_edited.json", sim->agent);
   // GetMotionJson("/Users/yoonbyung/Dev/mujoco_mpc/mjpc/tasks/common_rig/common_rig_v2_walk.json", sim->agent);
 
   sim->agent->plan_enabled = absl::GetFlag(FLAGS_planner_enabled);
